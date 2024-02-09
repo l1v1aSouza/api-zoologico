6f4fade977055a872b21866becc98364cd72fb3a
Lívia Souza <livia.souza10@aluno.senai.br>
2024-02-09 13:55:22 -0300

    Adicionei os comentários

--- src/model/Ave.ts
+++ src/model/Ave.ts
@@ -1,41 +1,41 @@
 import { Animal } from "./Animal";
 
 export class Ave extends Animal{
-    /**
-     * Representa a envergadura da ave.
-     */
-    private envergadura: number;
+/**
+ * Representa a envergadura da ave.
+*/
+private envergadura: number;
     
-    /**
-    * Construtor da classe Ave, que herda características de uma classe pai (superclasse).
-    * 
-    * @param _envergadura A envergadura da ave, que é uma característica específica das aves.
-    * @param _nome O nome da ave, uma informação básica sobre a ave.
-    * @param _idade A idade da ave, que pode ser relevante em contextos diversos.
-    * @param _genero O gênero da ave, como "macho", "fêmea" ou "indeterminado".
-    */
-    constructor(_envergadura: number, _nome: string, _idade: number, _genero: string){
-        // Chama o construtor da superclasse (classe pai), que é a classe da qual Ave herda propriedades e métodos.
-        super(_nome, _idade, _genero);
-        // Atribui a envergadura fornecida ao atributo 'envergadura' específico da classe Ave.
-        this.envergadura = _envergadura;
-    }
+/**
+ * Construtor da classe Ave, que herda características de uma classe pai (superclasse).
+ * 
+ * @param _envergadura A envergadura da ave, que é uma característica específica das aves.
+ * @param _nome O nome da ave, uma informação básica sobre a ave.
+ * @param _idade A idade da ave, que pode ser relevante em contextos diversos.
+ * @param _genero O gênero da ave, como "macho", "fêmea" ou "indeterminado".
+*/
+constructor(_envergadura: number, _nome: string, _idade: number, _genero: string){
+    // Chama o construtor da superclasse (classe pai), que é a classe da qual Ave herda propriedades e métodos.
+    super(_nome, _idade, _genero);
+    // Atribui a envergadura fornecida ao atributo 'envergadura' específico da classe Ave.
+    this.envergadura = _envergadura;
+}
 
-    /**
-     * Retorna a envergadura do animal
-     * 
-     * @returns envergadura : envergadura do animal
-     */
-    public getEnvergadura(): number {
-        return this.envergadura;
-    }
+/**
+ * Retorna a envergadura do animal
+ * 
+ * @returns envergadura : envergadura do animal
+ */
+public getEnvergadura(): number {
+    return this.envergadura;
+}
 
-    /**
-     * Atribui o parâmetro ao atributo envergadura
-     * 
-     * @param _envergadura : envergadura do animal
-     */
-    public setEnvergadura(_envergadura: number): void {
-        this.envergadura = _envergadura;
-    }
+/**
+ * Atribui o parâmetro ao atributo envergadura
+ * 
+ * @param _envergadura : envergadura do animal
+*/
+public setEnvergadura(_envergadura: number): void {
+    this.envergadura = _envergadura;
+}
 }
